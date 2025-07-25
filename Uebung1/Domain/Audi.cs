870eafe4573317c12012d22fee6c85ae9e850108
cakir <hamza.cakir@leue.de>
2025-07-25 16:10:08 +0200

    first session

---
+++ Uebung1/Domain/Audi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uebung1.Domain;
+
+public class Audi : Auto
+{
+    public Audi(string farbe, int maximaleGeschwindigkeit) : base(farbe, maximaleGeschwindigkeit)
+    {
+    }
+}
---
+++ Uebung1/Domain/Auto.cs
@@ -0,0 +1,14 @@
+﻿namespace Uebung1.Domain;
+
+public class Auto
+{
+    public string Farbe { get; set; }
+    public int MaximaleGeschwindigkeit { get; set; }
+    public int AnzahlTueren { get; set; }
+
+    public Auto(string farbe, int maximaleGeschwindigkeit)
+    {
+        Farbe = farbe;
+        MaximaleGeschwindigkeit = maximaleGeschwindigkeit;
+    }
+}
